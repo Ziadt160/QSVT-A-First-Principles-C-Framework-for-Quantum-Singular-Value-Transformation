c11f12da2b3838b4a808588e9db9ebfbdf940e83
Ziadt160 <ziadt160@gmail.com>
2025-10-26 00:23:49 +0300

    Delete build

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ziad/QSVT_Project
-# Build directory: /home/ziad/QSVT_Project/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("tests")
--- build/src/CMakeFiles/qsvt_lib.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for qsvt_lib.
-# This may be replaced when dependencies are built.
